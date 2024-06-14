157cfe22276ee7e443760c8c013ad556ffd87c87
Learner Getready <learnergit@hymyilee.fi>
2024-06-14 17:47:51 +0300

    logging

--- utils/logger.js
+++ utils/logger.js
@@ -1,5 +1,5 @@
-const info = (...params) => process.env.NODE_ENV !== 'test' ? console.log(...params) : null
-const error = (...params) => process.env.NODE_ENV !== 'test' ? console.error(...params) : null
+const info = (...params) => process.env.NODE_ENV !== 'test' ? console.log(...params) : console.log(...params)
+const error = (...params) => process.env.NODE_ENV !== 'test' ? console.error(...params) : console.error(...params)
 
 module.exports = {
   info,
